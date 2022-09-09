b3b392c65c1e33acc4e65cc9a3d99606bc237f5b
TheChaNiel <harryniel2019@gmail.com>
2022-09-09 04:00:11 -0700

    Mandatory

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
-#!/bin/bash                                                                
-#include <stdio.h>                                                         
-/**                                      
- * main - prints the phrase "Programming is like building a multilingual puzzle         
- * Return: 0 if exited properly, non-zero otherwise                  
- */                                                                        
-int main(void)                  
-{                                                                          
-	        puts("\"Programming is like building a multilingual puzzle");return (0);
+#!/bin/bash
+#include <stdio.h>
+
+/** 
+ *
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building multilingual puzzle");
+	return (0);
 }
+
