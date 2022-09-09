306396e6fda9f93a534d3d14faad9574ec9c3968
TheChaNiel <harryniel2019@gmail.com>
2022-09-09 13:44:50 -0700

    Mandatory

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
-/** 
- *
- * Description: using the main function
- * this program prints "Programming is like building a multilingual puzzle
- * Return: 0
+#include <studio.h>
+/**
+ * main - print the prase "Programming is like building a multilingual puzzle
+ * Return: 0 if exiled properly, non-zero otherwise
  */
 int main(void)
 {
-	puts("\"Programming is like building multilingual puzzle");
-	return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0)
