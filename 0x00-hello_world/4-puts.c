b56fa4ea7414fdce904cf946cd13e3582659217b
TheChaNiel <harryniel2019@gmail.com>
2022-09-09 17:49:21 -0700

    Mandatory

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
-#include <studio.h>
-/**
- * main - print the prase "Programming is like building a multilingual puzzle
- * Return: 0 if exiled properly, non-zero otherwise
- */
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0)
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	return (0);
+}
