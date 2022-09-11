d79c8d4358e7f82e8c221d0700e7273b9ce02308
TheChaNiel <harryniel2019@gmail.com>
2022-09-11 14:37:41 -0700

    Mandatory

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - print if the number is positive, zero, or negative
+ * main - Entry point
  *
  * Description: using the main function
  * this program prints "Programming is positive, zero, or negative
