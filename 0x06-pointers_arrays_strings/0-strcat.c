4fd18a40df3dad48119704005916adb0a48acd07
TheChaNiel <harryniel2019@gmail.com>
2022-09-22 12:18:14 -0700

    Mandatory

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,11 +5,10 @@
  * the end of the string pointed to by @dest
  * @dest: String that will be appended
  * @src: String to be concatenated upon
- *
  * Return: returns pointer to @dest
  */
 
-char* _strcat(char*dest, char*src)
+char *_strcat(char *dest, char *src)
 {
 	int index = 0, dest_len = 0;
 
